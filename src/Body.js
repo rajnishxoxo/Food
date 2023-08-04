a7cd35fff91611b8b8636b7c2aa6448cbcb11719
Rajnish <srajnish235.rs@gmail.com>
2023-08-04 23:51:57 +0530

    Implemented Search

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,8 @@ import Shimmer from "./Shimmer.js";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurant] = useState(restaurantList);
 
+  const [filterRestro,setFilterRestro]=useState(restaurantList);
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -15,26 +17,53 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.2893144&lng=80.4604643&is-seo-homepage-enabled=true"
     );
     const json = await data.json();
-    console.log(
-      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants[0]
-        .info
-    );
 
     setListOfRestaurant(
       json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+
+    setFilterRestro( json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   };
 
   if (listOfRestaurants.length === 0) {
     return <Shimmer />;
   }
 
+  const [value, setValue] = useState("");
+
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleClick=()=>{
+
+    let filterData = listOfRestaurants.filter((data)=>{
+      return data.info.name.toLowerCase().includes(value.toLowerCase());
+    
+    })
+    setFilterRestro(filterData)
+  }
+
   return (
-    <div className="restaurant-list">
-      {listOfRestaurants.map((restaurant) => {
-        return <RestaurantCard key={restaurant.id} {...restaurant.info} />;
-      })}
-    </div>
+    <>
+      <div className="searchBox">
+        <input
+          className="searchBar"
+          type="text"
+          placeholder="Search For Restaurant"
+          value={value}
+          onChange={handleOnChange}
+        />
+        <button className="btn" onClick={handleClick}>
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {filterRestro.map((restaurant) => {
+          return <RestaurantCard key={restaurant.id} {...restaurant.info} />;
+        })}
+      </div>
+    </>
   );
 };
 
