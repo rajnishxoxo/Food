b30b4d7d3cf13b61d5d7f7d2742045430d7a75c1
Rajnish <srajnish235.rs@gmail.com>
2023-08-04 14:16:20 +0530

    Shimmer.js
    Created Shimmer Card

---
+++ src/Shimmer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Shimmer =()=>{
+    return(
+        <div className="shimmer-Container">
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+        </div>
+    )
+}
+
+export default Shimmer
