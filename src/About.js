cc1bf242175f4647efeae6213bfac30db12b396e
Rajnish <srajnish235.rs@gmail.com>
2023-08-24 16:12:30 +0530

    Changed to Class based

--- src/About.js
+++ src/About.js
@@ -1,12 +1,27 @@
 import UserClass from "./UserClass";
 
-const About = ()=>{
-    return(
-        <div>
-            <h1>About us Page</h1>
-            <UserClass name="Rajnish" location="Pune-MH"/>
-        </div>
-    )
+import React from "react";
+
+class About extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    componentDidMount(){
+
+        console.log("Parent Mounted")
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>About us Page</h1>
+                <UserClass name="Rajnish" location="Pune-MH" />
+            </div>
+        )
+    }
 }
+
 
 export default About;
