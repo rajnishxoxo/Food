5e8d515468fcf9c4696fa3e0e66939b8b7884c82
Rajnish <srajnish235.rs@gmail.com>
2023-08-25 11:31:14 +0530

    About.js
    Adding Para and CSS to About us Section

--- src/About.js
+++ src/About.js
@@ -3,12 +3,10 @@ import UserClass from "./UserClass";
 import React from "react";
 
 class About extends React.Component {
- 
-
   render() {
     return (
-      <div>
-        <p className="about-us-Section">
+      <div className="about-us-Section">
+        <p className="intro-Para">
           Welcome to Swiggy, your go-to destination for delicious food delivered
           right to your doorstep. At Swiggy, we're on a mission to revolutionize
           the way you experience food delivery. Our platform brings together a
@@ -16,8 +14,8 @@ class About extends React.Component {
           top-notch delivery services to create an unparalleled dining
           experience.
         </p>
-        <h3> Our Mission</h3>
-        <p>
+        <h3 className="heading-Mission"> Our Mission</h3>
+        <p className="intro-Para">
           Our mission is simple: to connect food enthusiasts with the flavors
           they love, all while supporting local businesses. We're committed to
           delivering not just food but also convenience, choice, and moments of
